657fd7985b1b3177b659b82dcb35baf40de396f7
j-e06 <jani.eraluoto@metropolia.fi>
2024-09-18 09:06:46 +0300

    code refactoring (still is horrible though), next step would be to figure out goals for airports ig

--- main.py
+++ main.py
@@ -51,62 +51,73 @@ def update_game(game_id, location, money, range):
     sql = f"UPDATE game SET money = {money}, location = '{location}', player_range = {range} WHERE id = {game_id}"
     global cursor
     cursor.execute(sql)
-def fly(nykyinen,koodi):
-    global cursor
-    if koodi not in [airport[1] for airport in airports]:
-        print("Virheellinen koodi")
-        return
 
-    #koodi löytyy.
+def fly(nykyinen):
+    global cursor
+    koodi = input("Mihin lennetään (ICAO-koodi): ")
+
+    if koodi not in [airport[1] for airport in airports]:
+        return False
+
     matka = get_distance(nykyinen, koodi)
-    update_game(peli, koodi, money, player_range - matka)
-    return True
+    if player_range>matka>1:
+        update_game(peli, koodi, money, player_range - matka)
+        return True
+
+def accessible_airports(nykyinen, player_range):
+    test = 0
+    for kenttä in airports:
+        matka = get_distance(nykyinen, kenttä[1])
+        if player_range > matka > 1:
+            print(f"{kenttä[2]} (ICAO: {kenttä[1]}) | Matka: {matka:.2f} km")
+            test += 1
+    return test
 name = input("Pelaajan nimi: ")
 airports = get_airports()
 starting_location = random.choice(airports)
 current_airport = starting_location
 money = 1000
 player_range = 1500
-
 peli = create_game(name, starting_location, money, player_range)
-#peli = 1
 while True:
     #peli loop
-    print(current_airport[2])
-    print(money)
-    print(player_range)
+    print(f"Nykyinen lentokenttä: {current_airport[2]}")
+    print(f"Rahaa jäljellä: {money} €")
+    print(f"Jäljellä oleva lentomatka: {player_range} km\n")
     print("""Vaihtoehdot
     1. Tankkaa
     2. Lähde matkalle""")
-    tehtava = int(input("Mitä tehdään: "))
+    tehtava = int(input("\nMitä tehdään: "))
 
     if tehtava == 1:
         maara = int(input("Paljonko rahaa haluat käyttää? (1 euro = 2 kilometriä.) "))
-        if maara > money < 0:
-            print("Jotain meni pieleen.")
-            continue
-        else:
+        if money > maara and maara > 0:
             player_range += maara * 2
             money -= maara
             update_game(peli, current_airport[1], money, player_range)
             print("Tankkaus onnistui")
-    elif tehtava == 2:
-        print("Lentokenttä vaihtoehdot:")
-        for airport in airports:
-            # jos distance on yli mitä meillä on, älä näytä koska sinne ei päästä
-            pituus = get_distance(current_airport, airport[1])
-            if player_range > pituus > 1:
-                print(airport[2], "ICAO:", airport[1], f"Matka: {pituus}")
-        koodi = input("Mihin lennetään: ")
-        result = fly(current_airport, koodi)
-        if not result:
-            print("Virheellinen koodi")
-            continue
         else:
-            current_airport = get_airport_info(koodi)
-            print("Lento onnistui")
+            print("Jotain meni pieleen.")
+            continue
+    elif tehtava == 2:
+        #testataan että voinko tehdä tämän logiikan FLY funktiossa
+        test = accessible_airports(current_airport, player_range)
+        if test == 0:
+            print("Ei ole mahdollista lentää mihinkään. Tankkaa lisää.")
+            continue
+        print("Lentokenttä vaihtoehdot:")
+
+        fly_result = fly(current_airport)
+        if fly_result:
+            # tallennetaan nykyinen sijainti
+            current_airport = get_airport_info(fly_result)
+        else:
+            print("Virheellinen syöte tai väärä ICAO-koodi.")
+    elif tehtava == 3:
+        # lootbox
+
+        pass
     game_info = get_game(peli)
-    print(game_info)
     money = game_info[1]
     current_airport = get_airport_info(game_info[2])
     player_range = game_info[4]
