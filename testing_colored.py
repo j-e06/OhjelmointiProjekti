bb4ae6f80842ed662c1fc420343a57a8bfccfa9b
j-e06 <jani.eraluoto@metropolia.fi>
2024-10-07 17:56:21 +0300

    Delete testing_colored.py

--- testing_colored.py
+++
@@ -1,9 +0,0 @@
-
-from colored import Fore, Back, Style
-
-def output(type_of_text,text, color_info):
-    #assuming i pass in print, and input
-    #how would i pass out the color info?
-    if type_of_text == "input":
-        result = input(Fore.GREEN + text + Style.RESET_ALL)
-    pass
